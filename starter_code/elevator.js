114c08a668e26af98a2c5dace53da2b945915c70
JPZ <jeanpaulzune@gmail.com>
2017-02-20 23:05:07 -0500

    Committing out of habit. Working Iteration 4. Have not tested...

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,64 @@
 class Elevator {
-  constructor(){
+    constructor(){
     this.floor      = 0;
     this.MAXFLOOR   = 10;
+    this.direction = 'up';
+    this.waitingList = [];
+    this.passengers = [];
     this.requests   = [];
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() {
+    // initiate interval every second
+  this.interval = setInterval(() => { this.update(); }, 1000);
+  }
+
+// clears intervals
+  stop() {
+    clearInterval(interval);
+   }
+
+  update() {
+    this.log();
+    console.log();
+   }
+
+  _passengersEnter(person) {
+    this.passengers.push(person);
+    indexOfPerson = passengers.indexOf(person);
+    this.requests.push(this.Person[indexOfPerson].destinationFloor);
+    this.waitingList.splice(indexOfPerson,1);
+    console.log(`${Person.name} has enter the elevator`);
+   }
+
+  _passengersLeave(person) {
+    indexOfPerson = passengers.indexOf(person);
+    if (Person.destinationFloor === Person.originFloor) {
+    passengers.splice(indexOfPerson,1);
+    console.log(`${Person.name} has left the elevator`);
+    }
+  }
+
+  floorUp() {
+    if (this.floor <= this.MAXFLOOR)
+    this.floor ++;
+
+   }
+
+  floorDown() {
+    if (this.floor >= 0)
+    this.floor --;
+   }
+
+
+  call(originFloor) {
+    this.requests.push(originFloor);
+    this.waitingList.push(originFloor);
+   }
+
+  log() {
+    console.log(`Direction: ${this.direction} | Floor: ${this.floor}`);
+   }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,19 @@
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+
+const myElevator = new Elevator();
+
+const mary = new Person('Maria', 2, 8);
+const sabine = new Person('Sabine', 7, 1);
+const alejandra = new Person('Alejandra', 4, 6);
+const baruska = new Person('Baruska', 4, 6);
+
+
+// myElevator.start();
+// myElevator.floorUp();
+// myElevator.floorUp();
+// myElevator.floorUp();
+myElevator.call(2, 3);
+
+// myElevator.floorDown();
+myElevator.log();
